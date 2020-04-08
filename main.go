15b4605e3faa805ff2d805a2ce5087182aca8a8e
usrbinkat <usrbinkat@braincraft.io>
2020-04-07 21:11:43 -0600

    prompt-in-progress

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import "github.com/containercraft/p1-gotools/src/text"
+import "github.com/containercraft/p1-gotools/src/prompt"
+import "github.com/containercraft/p1-gotools/src/confirm"
 
 func main() {
     print("\033[H\033[2J")
     text.PrintIntro()
+    confirm.PromptContinue()
+    prompt.WhichCity()
 }
 
