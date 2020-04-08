0e08ee49d6c1d88e84184b73f99833f22e6a0748
usrbinkat <usrbinkat@braincraft.io>
2020-04-08 17:30:24 +0000

    readability

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import "github.com/containercraft/p1-gotools/src/text"
-import "github.com/containercraft/p1-gotools/src/prompt"
-import "github.com/containercraft/p1-gotools/src/confirm"
+import (
 
-import "fmt"
-import "os"
+        "os"
+        "fmt"
+
+        "github.com/containercraft/p1-gotools/src/text"
+        "github.com/containercraft/p1-gotools/src/prompt"
+        "github.com/containercraft/p1-gotools/src/confirm"
+
+)
 
 var clear = "\033[H\033[2J"
 func main() {
+
+    // clear screen
     print(clear)
+
+    // Print Intro TexBlock to Screen
     text.PrintIntro()
     fmt.Print("    Continue? (Yes/No): ")
     runContinue := confirm.PromptContinue()
     if runContinue != true {
 	    os.Exit(1)
     }
+
+    // Collect VPC {name,cluster subdomain,domain}
     prompt.VarsVpc()
 }
